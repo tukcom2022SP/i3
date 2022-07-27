a1b530a93efa600340b2005db07c84838d75daef
Jisssuu <wkdwltn1015@naver.com>
2022-07-27 14:02:40 +0900

    basic

---
+++ i3_project/i3_project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  i3_project
+//
+//  Created by 홍길동 on 2022/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ i3_project/i3_project/i3_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  i3_projectApp.swift
+//  i3_project
+//
+//  Created by 홍길동 on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct i3_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
