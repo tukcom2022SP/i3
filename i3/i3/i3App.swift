c48efc584a958e00e85690f44c98a0581075fceb
Jisssuu <wkdwltn1015@naver.com>
2022-07-27 13:51:53 +0900

    basic

---
+++ i3/i3/i3App.swift
@@ -0,0 +1,17 @@
+//
+//  i3App.swift
+//  i3
+//
+//  Created by 홍길동 on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct i3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
