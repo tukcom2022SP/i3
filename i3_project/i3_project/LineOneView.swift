0ac5d4427715f057c73a12f52df3ee9083d4cba6
Jisssuu <wkdwltn1015@naver.com>
2022-08-02 14:55:41 +0900

    line1 searchBar update

--- i3_project/i3_project/LineOneView.swift
+++ i3_project/i3_project/LineOneView.swift
@@ -8,21 +8,59 @@
 
 import SwiftUI
 
+struct searchBar: View {
+    @Binding var text : String
+    @State var editText : Bool = false
+    var body: some View {
+        HStack{
+            TextField("검색어를 넣어주세요" , text : self.$text)
+                .padding(15)
+                .padding(.horizontal,15)
+                .background(Color(.systemGray6))
+                .cornerRadius(15)
+                .overlay(
+                    HStack{
+                        Spacer()
+                        if self.editText{
+                              Button(action : {
+                                self.editText = false
+                                self.text = ""
+                                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                            }){
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(Color(.black))
+                                    .padding()
+                            }
+                        }else{
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(Color(.black))
+                                .padding()
+                        }
+                       
+                    }
+                ).onTapGesture {
+                    self.editText = true
+                }
+        }
+    }
+}
+
 struct LineOneView: View {
+    let searchController = UISearchController(searchResultsController: nil)
+    @State var text = ""
     @ObservedObject var lineStore = LineStoreOne.singleton
     var body: some View {
-        NavigationView {
-            List {
-                ForEach (lineStore.saturday, id:\.stationCount) { line in
+            searchBar(text: $text)
+        List {
+            ForEach (lineStore.saturday.filter{"\($0)".contains(self.text) || text == ""}, id:\.stationCount) { line in
                     NavigationLink (destination: LineDetailView(line:line)) {
                         LineOneStationView(line: line)
                     }
                 }
             }
-            .navigationTitle(Text("Line1"))
-        }.onAppear {
+            .onAppear {
             lineStore.load()
-        }
+            }
     }
 }
 
