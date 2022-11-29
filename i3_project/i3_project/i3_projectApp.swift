5c385ed2ba0a88382c984e37fdc35c33354f59fe
antonio26548 <psein1007@gmail.com>
2022-11-29 12:59:12 +0900

    rebuilding

--- i3_project/i3_project/i3_projectApp.swift
+++ i3_project/i3_project/i3_projectApp.swift
@@ -1,12 +1,10 @@
 import SwiftUI
 //import FirebaseDatabase // 추가됨
-import FirebaseCore
-import Firebase //추가됨
+
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
       
      // let ref = FIRDatabase.database().reference()  //test
      // let itemsRef = ref.child("TEST")  // test
@@ -18,9 +16,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
 @main
 struct i3_projectApp: App {
-    init() {
-          FirebaseApp.configure() // 코드 추가
-      }
     var body: some Scene {
         WindowGroup {
            LoginView()
